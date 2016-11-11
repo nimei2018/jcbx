67297fc51813f02e53d68607e9b557e585f9d2a7
fucknimei <fuck@nimei.com>
2016-11-11 19:05:19 +0800

    update 20161111_190519_5

---
+++ https.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+# File Name: https.py
+# Created Time: 2016年11月11日 星期五 18时59分34秒
+# Mail: Jason Bourne@nimei.com
+# Author: Jason Bourne
+
+
+https://imququ.com/post/web-security-and-response-header.html#toc-0
+
+索引：
+http://op.aidu.com/2015/04/https-index/
+
+大型网站的 HTTPS 实践（一）—— HTTPS 协议和原理
+http://op.aidu.com/2015/04/https-s01a01/
+
+大型网站的 HTTPS 实践（二）——HTTPS 对性能的影响
+http://op.aidu.com/2015/04/https-s01a02/
+
+大型网站的 HTTPS 实践（三）——基于协议和配置的优化
+http://op.aidu.com/2015/04/https-s01a03/
+
+大型网站的 HTTPS 实践（四）——协议层以外的实践
+http://op.aidu.com/2015/04/https-s01a04/
+
+http://www.weidu8.net/wx/1007147882430299
+
+http://www.alloyteam.com/2012/03/web-cache-2-browser-cache/
